971993056ed44d2b35762615c40a5909e17b60a6
Radoslaw Czajkowski <radoslaw.cz@gmail.com>
2023-06-29 11:21:00 +0000

    Add new storage account to main.tf

--- main.tf
+++ main.tf
@@ -26,3 +26,10 @@ resource azurerm_storage_account sa {
   account_replication_type = GRS
 }
 
+resource "azurerm_storage_account" "example" {
+  name                     = "storagelypgjun407570li8"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
